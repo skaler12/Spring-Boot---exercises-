f8b5c5188ff524c3842a5c587c3f0cf020afb39b
skaler12 <gpoznanski98@gmail.com>
2020-10-03 23:51:44 +0200

    first commit, create project

---
+++ src/main/java/pl/poznanski/spring_boot_exercises/SpringBootExercisesApplication.java
@@ -0,0 +1,13 @@
+package pl.poznanski.spring_boot_exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/poznanski/spring_boot_exercises/SpringBootExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.poznanski.spring_boot_exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
